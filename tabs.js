faeb1ffff965245245cd43d8dbb25051a533ecec
Fábio Lima <fabiolima@github.com>
2021-07-02 12:16:19 -0300

    First

--- tabs.js
+++ tabs.js
@@ -16,41 +16,4 @@
  * - Atestar a proeficiência do candidato em Javascript nativo e manipulação do DOM.
  *
  */
-class Tabs {
-  id;
-  $rootElement;
-
-  constructor(id) {
-    this.id = id;
-    this.$rootElement = document.getElementById(id);
-
-    this.bindListeners(this.$rootElement)
-  }
-
-  bindListeners(tabsComponent) {
-    const tabs = tabsComponent.getElementsByClassName('tab')
-
-    for (let tab of tabs) {
-      tab.addEventListener('click', (e) => this.onTabClick(tab))
-    }
-
-  }
-
-  onTabClick(newActiveTab) {
-    const currentActiveTab = this.$rootElement.getElementsByClassName('tab active')[0]
-    currentActiveTab.classList.remove('active')
-
-    const currentActiveContent = this.$rootElement.getElementsByClassName('tab-content active')[0]
-    currentActiveContent.classList.remove('active')
-
-    const newActiveContent = document.getElementById(newActiveTab.dataset.target)
-
-    newActiveTab.classList.add('active')
-    newActiveContent.classList.add('active')
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const tabs = new Tabs('my-tabs')
-})
-
+class Tabs {}
